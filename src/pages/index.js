0a15c05f4def653c7b894babd30f48c7103eacba
sebastian Hoyos Lopez <sebastiahoyos145@gmail.com>
2022-07-05 14:27:24 -0500

    Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,9 +16,25 @@ const AboutSchool = () => {
   return (
     <>
       <div>
-        <Typography variant="h1" sx={{ color: 'white', position: 'absolute', zIndex: '1', width: '100%', marginTop: '80px', marginLeft: '2rem' }}>
-          Lenguas nativas
-        </Typography>
+        <div style={{
+          color: 'white',
+          position: 'absolute',
+          zIndex: '1',
+          width: '80%',
+          marginTop: '80px',
+          marginLeft: '2rem'
+        }}>
+          <Typography variant="h1">
+            Lenguas nativas
+          </Typography>
+          <div style={{ textAlign: "justify", marginLeft: "10rem", background: "black", opacity: 0.5}}>
+            <Typography variant='p' fontSize={20}>
+              Esta página web es resultado de dos proyectos de revitalización lingüística realizados en el departamento de Amazonas, bajo la asesoría de la Escuela Taller de Boyacá y el apoyo de las direcciones de Poblaciones y de Patrimonio del Ministerio de Cultura. La página contiene informaciones recogidas entre los meses de diciembre de 2021 y abril de 2022 en dos lugares diferentes: en la Comunidad píínemuna (bora) de Nueva Esmeralda (localizada en el curso inferior del río Igaraparaná), y en el Cabildo de Pueblos Indígenas Unidos de Leticia, Capiul, cuyo carácter es multiétnico. El lector podrá encontrar aquí cantos de varios pueblos amazónicos con sus respectivos vocabularios; un resumen de los principales contenidos abordados durante los talleres de lenguas, e imágenes y videos alusivos a algunas manifestaciones culturales de las comunidades visitadas. El material fotográfico proviene del archivo de la investigadora Camila Sofía Venegas Osorio. La edición y selección de textos corrió a cargo del investigador Gustavo Zuluaga Hoyos
+            </Typography>
+
+          </div>
+
+        </div>
         <div>
           <Image src={imagenPresentacion} alt="Imagen presentación" />
         </div>
