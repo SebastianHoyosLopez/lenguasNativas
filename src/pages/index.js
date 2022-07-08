a0e04aa22c004ccdaa5e6de140538e37e4b897f3
sebastian Hoyos Lopez <sebastiahoyos145@gmail.com>
2022-07-08 08:25:47 -0500

    Se agrega la galeria

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,8 @@
 import Image from 'next/image';
 import imagenPresentacion from '../../public/image/portadaHome.png';
 import { Box, Grid, Typography } from '@mui/material';
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import Head from 'next/head';
-import Post from '../components/Post';
-import { sortByDate } from '../utils';
 
-export default function Home({ posts }) {
+export default function Home() {
   return (
     <>
       <Box sx={{ padding: '0 3rem 0 3rem' }}>
@@ -34,42 +28,6 @@ export default function Home({ posts }) {
           </Grid>
         </Grid>
       </Box>
-      <Head>
-        <title>Dev Blog</title>
-      </Head>
-
-      <div className="posts">
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
-      </div>
     </>
   );
-}
-
-export async function getStaticProps() {
-  // Get files from the posts dir
-  const files = fs.readdirSync(path.join('posts'));
-
-  // Get slug and frontmatter from posts
-  const posts = files.map((filename) => {
-    // Create slug
-    const slug = filename.replace('.md', '');
-
-    // Get frontmatter
-    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8');
-
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      frontmatter,
-    };
-  });
-
-  return {
-    props: {
-      posts: posts.sort(sortByDate),
-    },
-  };
 }
