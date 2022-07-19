df32d84eb83dc502ac83c3370a9e8b5c190264e3
sebastian Hoyos Lopez <sebastiahoyos145@gmail.com>
2022-07-19 14:09:19 -0500

    Se agrega video faltante en comunidad esmeralda

--- src/pages/Comunidad_nueva_esmeralda/MultimediaVideo.js
+++ src/pages/Comunidad_nueva_esmeralda/MultimediaVideo.js
@@ -4,12 +4,15 @@ import ReactPlayer from 'react-player';
 const MultimediaVideo = () => {
   return (
     <>
-      <div style={{ display: 'flex', justifyContent: 'space-around', marginRight: '5rem', marginLeft: '5rem', marginBottom: '5rem' }}>
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginRight: '5rem', marginLeft: '5rem', marginBottom: '5rem', marginTop: '5rem' }}>
         <div style={{ width: '40%', height: '20rem' }}>
-          <ReactPlayer url="https://www.youtube.com/watch?v=d6JXul6JNL0" width="100%" height="100%" />
+          <ReactPlayer url="https://www.youtube.com/watch?v=d6JXul6JNL0" width="95%" height="100%" />
         </div>
         <div style={{ width: '40%', height: '20rem' }}>
-          <ReactPlayer url="https://www.youtube.com/watch?v=ltD49Fo9F9Y" width="100%" height="100%" />
+          <ReactPlayer url="https://www.youtube.com/watch?v=ltD49Fo9F9Y" width="95%" height="100%" />
+        </div>
+        <div style={{ width: '40%', height: '20rem', display: 'flex' }}>
+          <ReactPlayer url="https://www.youtube.com/watch?v=CzXSaqU9Z1I&ab_channel=Sof%C3%ADaVenegas" width="95%" height="100%" />
         </div>
       </div>
     </>
